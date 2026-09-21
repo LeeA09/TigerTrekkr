a96831542730d5f6c701abd78c261498973ce463
Isaiah-Korte-05 <ijkmmf@gmail.com>
2026-09-21 15:13:03 -0500

    changed browser icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
+
+	// import favicon from '$static/rubber-duck.ico';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/rubber-duck.ico" />
 </svelte:head>
 
 {@render children()}
